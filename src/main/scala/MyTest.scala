0bd72350473d12bbe166044ffb6afa4290d84aa9
Guo <guoch@tab112147.corpad.net.local>
2017-10-30 16:08:15 +1100

    add hdfs config xml

--- src/main/scala/MyTest.scala
+++ src/main/scala/MyTest.scala
@@ -1,5 +1,12 @@
 import com.twitter.scalding._
 
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import org.apache.hadoop.conf._
+import org.apache.hadoop.fs._
+
 /**
   * Created by guoch on 19/10/17.
   */
@@ -12,8 +19,13 @@ import com.twitter.scalding._
 //}
 
 class WordCountJob(args : Args) extends Job(args) {
+
+  private val conf = new Configuration()
+
+
   TextLine(args("input"))
     .flatMap('line -> 'word) {line : String => line.toLowerCase.split("\\s+")}
     .groupBy('word) {_.size}
     .write(Tsv(args("output")))
+
 }
